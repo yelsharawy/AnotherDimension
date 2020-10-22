940f4b8ca7b1a08c81dd593d629287e77629fd55
yelsharawy <yelsharawy20@stuy.edu>
2020-10-22 13:04:06 -0400

    Added & tested sum method

--- ArrayOps.java
+++ ArrayOps.java
@@ -1,4 +1,11 @@
 public class ArrayOps {
 
-    
+    public static int sum(int[] arr) {
+        int total = 0;
+        for (int num : arr) {
+            total += num;
+        }
+        return total;
+    }
+
 }
--- Tester.java
+++ Tester.java
@@ -1,7 +1,8 @@
 public class Tester {
 
     public static void main(String[] args) {
-
+        int[] test1DArr = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        System.out.println(ArrayOps.sum(test1DArr)); // 55
     }
-    
+
 }
